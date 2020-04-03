9a896d2ab44af01a6ced1535556c56ca781a629c
MashaBut <mashutkabut99@gmail.com>
2020-04-03 20:19:29 +0300

    add RSA

---
+++ RSA/RSA/IO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    class IO
+    {
+        public static string EnterText(string message)
+        {
+            string result = null;
+
+            do
+                Console.WriteLine(message);
+            while ((result = Console.ReadLine().Trim()) == string.Empty);
+
+            return result;
+        }
+
+        public static int EnterNumber(string message)
+        {
+            int num = 0;
+
+            do
+                Console.Write(message);
+            while (!int.TryParse(Console.ReadLine(), out num));
+
+            return num;
+        }
+    }
+}
---
+++ RSA/RSA/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace RSA
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (!RSA.GetKeysAndN(out BigInteger publicKey, out BigInteger privateKey, out BigInteger n))
+                {
+                    Console.WriteLine($"Ошибка: не удалось сгенерировать простые числа за { RSA.MaxIterationCount } попыток");
+                    Console.WriteLine("Нажмите Escape, чтобы выйти или любую клавишу, чтобы повторить");
+
+                    if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                        break;
+
+                    continue;
+                }
+
+                Console.WriteLine("N: " + n);
+                Console.WriteLine("Открытый ключ: " + publicKey);
+                Console.WriteLine("Закрытый ключ: " + privateKey);
+
+                Console.WriteLine();
+
+                string text = IO.EnterText("Введите шифруемый текст:");
+
+                List<BigInteger> result = RSA.EncriptRSA(text, publicKey, n);
+
+                Console.WriteLine("Шифрованное сообщение:");
+
+                foreach (var item in result)
+                    Console.WriteLine(item);
+
+                Console.WriteLine();
+                Console.WriteLine("Нажмите любую клавишу, чтобы расшифровать");
+                Console.ReadKey();
+
+                text = RSA.DecriptRSA(result, privateKey, n);
+
+                Console.WriteLine(text);
+
+                Console.WriteLine("Нажмите Escape, чтобы выйти или любую клавишу, чтобы повторить");
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                    break;
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
